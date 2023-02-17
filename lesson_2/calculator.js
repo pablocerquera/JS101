161219186470a794a14227727729a6092bb3c4b5
Winston Wolfe <pabz1038@gmail.com>
2023-02-17 15:01:11 -0700

    added a git ignore added pseudo.js and refracted calculator.js and added eslintre.yml

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -6,26 +6,53 @@
 
 const readline = require('readline-sync');
 
-console.log("Welcome to the Calculator!");
-
-console.log("What is the first Number?");
-let number1 = readline.question();
-
-console.log("What is the second Number?");
-let number2 = readline.question();
-
-console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
-let operation = readline.question();
-
-let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2)
-} else if (operation === '2') {
-  output = Number(number1) - Number(number2)
-} else if (operation === '3') {
-  output = Number(number1) * Number(number2)
-} else if(operation === '4') {
-  output = Number(number1) / Number(number2)
+function prompt(msg) {
+  console.log(`=> ${msg}`);
 }
 
-console.log(`The result is ${output}.`)
+function invalidNumber(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
+
+prompt("Welcome to the Calculator!");
+
+prompt("What is the first Number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmmmm...that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt("What is the second Number?");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmmmm...that doesn't look like a valid number.");
+  number2 = readline.question();
+}
+
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt("You must choose 1, 2, 3, or 4.");
+  operation = readline.question();
+}
+
+let output;
+
+switch (operation) {
+  case '1' :
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
+}
+prompt(`The result is ${output}.`);
---
+++ lesson_2/pseudo.js
@@ -0,0 +1,16 @@
+// a method that takes an array of integers, and returns a new array with 
+// every other element from the original array, starting with the first 
+// element. For instance:
+Start 
+
+SET array of [1,4,7,2,5]
+SET iterator = 0
+SET new array = []
+WHILE iterator <= length of array 
+  push array[inticator] to new array 
+  iterator = iterator + 2
+
+PRINT new array
+
+end
+
