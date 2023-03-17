f84e8b5960d37f5d1a9ed3f1fd53d22ff92bdca7
Winston Wolfe <pabz1038@gmail.com>
2023-03-17 13:16:17 -0600

    "added exercises from the Hard1 portion of lesson 3."

---
+++ lesson_3/Hard1/question1.js
@@ -0,0 +1,17 @@
+function first() {
+  return {
+    prop1: "hi there"
+  };
+}
+
+function second() {
+  return
+  {
+    prop1: "hi there"
+  };
+}
+
+console.log(first());
+console.log(second());
+
+// first logs an Object the second one return undefined.
---
+++ lesson_3/Hard1/question4.js
@@ -0,0 +1,30 @@
+// input string should not have more or less than 4 components
+// You're not returning a false condition
+
+
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  if (dotSeparatedWords.length !== 4) {
+    return false;
+  }
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isAnIpNumber(str) {
+  if (/^\d+$/.test(str)) {
+    let number = Number(str);
+    return number >= 0 && number <= 255;
+  }
+
+  return false;
+}
+
+
+console.log(isDotSeparatedIpAddress('10.35.2.5'));
